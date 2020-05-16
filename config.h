212b132bbe058ad9ac05a3c9e51393d94c85c1bb
Wjinlei <1976883731@qq.com>
2020-05-17 04:43:57 +0800

    modify font to Jetbrains Mono

--- config.h
+++ config.h
@@ -16,8 +16,10 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=16" };
-static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=16";
+//static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=14" };
+//static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=14";
+static const char *fonts[]          = { "Jetbrains Mono:size=12" };
+static const char dmenufont[]       = "Jetbrains Mono:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -79,7 +81,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "100x28", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "80x23", NULL };
 static const char *browsercmd[]  = { "google-chrome-stable", NULL };
 static const char *raisevol[] = { "amixer", "set", "Master", "2+", NULL };
 static const char *lowervol[] = { "amixer", "set", "Master", "2-", NULL };
