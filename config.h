bbbea3926eaa134bab594110acc01ec3b1cbcd5f
Wjinlei <1976883731@qq.com>
2020-05-23 18:59:30 +0800

    modify tag style

--- config.h
+++ config.h
@@ -40,7 +40,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "Tag-1", "Tag-2", "Tag-3", "Tag-4", "Tag-5", "Tag-6", "Tag-7", "Tag-8", "Tag-9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
     /* xprop(1):
