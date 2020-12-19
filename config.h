d035fb9db9359aed5e9d178963f356c0c3d639dc
Wjinlei <1976883731@qq.com>
2020-12-19 15:50:18 +0800

    update

--- config.h
+++ config.h
@@ -16,8 +16,10 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "UbuntuMono Nerd Font:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true"  };
-static const char dmenufont[]       = "UbuntuMono Nerd Font:size=12";
+//static const char *fonts[]          = { "TerminessTTF Nerd Font Mono:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true"  };
+//static const char dmenufont[]       = "TerminessTTF Nerd Font Mono:size=12";
+static const char *fonts[]          = { "UbuntuMono Nerd Font Mono:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true"  };
+static const char dmenufont[]       = "UbuntuMono Nerd Font Mono:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#333333";
 static const char col_gray3[]       = "#bbbbbb";
